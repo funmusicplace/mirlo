13c8c906c6800c96c5ca4221916a2aae4b38163d
Si <simsamsomred@gmail.com>
2023-04-12 11:55:36 -0400

    chore: try to set correct back-end domain

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,5 @@
-export const API_ROOT = process.env.API_DOMAIN;
-
+export const API_ROOT = process.env.REACT_APP_API_DOMAIN;
+console.log("env", process.env);
 export const bp = {
   small: "430",
   medium: "800",
