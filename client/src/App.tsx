fdf6009b813fdc96ea9cd7531aa04699aa2867b2
Tim Glorioso <elegant.art2953@fastmail.com>
2023-10-14 22:14:26 -0400

    Cleanup global styles, prepare for deeper theme integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { css, injectGlobal } from "@emotion/css";
+import { css } from "@emotion/css";
+import { Global, css as reactCss } from "@emotion/react";
 import PageHeader from "components/common/PageHeader";
 import Snackbar from "components/common/Snackbar";
 import Player from "components/Player";
@@ -9,12 +10,9 @@ import { useGlobalStateContext } from "state/GlobalState";
 import SnackbarContext from "state/SnackbarContext";
 import useWidgetListener from "utils/useWidgetListener";
 import Header from "./components/Header/Header";
-import globalCSS from "./styles";
 import { Footer } from "components/Footer";
 import { bp } from "./constants";
 import { MetaCard } from "components/common/MetaCard";
-
-injectGlobal(globalCSS);
 
 function App() {
   const { state, dispatch } = useGlobalStateContext();
@@ -81,6 +79,179 @@ function App() {
         description="A music distribution and patronage site"
         image="/android-chrome-512x512.png"
       />
+      <Global styles={reactCss`
+        html {
+          --mi-black: #3E363F;
+          --mi-white: #FFEEDB;
+          --mi-pink: #BE3455;
+        
+          --mi-normal-background-color: var(--mi-white);
+          --mi-light-background-color: #ffffee;
+          --mi-lighten-background-color: rgba(255, 255, 255, 0.4);
+          --mi-shade-background-color: rgba(0, 0, 0, .05);
+        
+          --mi-normal-foreground-color: var(--mi-black);
+          --mi-light-foreground-color: #888;
+          --mi-lighter-foreground-color: #bbb;
+        
+          --mi-primary-color: var(--mi-pink);
+          --mi-primary-color--hover: var(--mi-pink);
+        
+          --mi-secondary-color: #ffb3d0;
+          --mi-secondary-color--hover: #FF80B0;
+        
+          --mi-success-background-color: #4cdb5f;
+        
+          --mi-warning-color: #f04e37;
+          --mi-warning-color--hover: #f26f59;
+          --mi-warning-background-color: var(--mi-warning-color);
+          --mi-darken-warning-background-color: #df250b;
+          --mi-warning-foreground-color: white;
+        
+          --mi-primary-highlight-color: #bcb3ff;
+          --mi-primary-highlight-color--hover: #FFB3D0;
+          --mi-box-color: var(--mi-secondary-color);
+        
+          --mi-border-radius: 4px;
+          --mi-border-radius-focus: 8px;
+          
+          --mi-icon-button-background-color: var(--mi-shade-background-color);
+          --mi-icon-button-background-color--hover: rgba(0, 0, 0, 0.2);
+        
+          --mi-font-family-stack: 'Arial', -apple-system, BlinkMacSystemFont,
+            'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+            'Droid Sans', 'Helvetica Neue', sans-serif;
+        }
+        
+        @media (prefers-color-scheme: dark) {
+          html {
+            --mi-normal-background-color: var(--mi-black);
+            --mi-normal-foreground-color: var(--mi-white);
+            --mi-lighten-background-color: rgba(255, 255, 255, 0.05);
+          }
+        }
+        
+        * {
+          box-sizing: border-box;
+          margin: 0;
+          padding: 0;
+        }
+        
+        html {
+          font-size: 20px;
+          min-height: 100%;
+        }
+        
+        body {
+          margin: 0;
+          background-color: var(--mi-normal-background-color);
+          color: var(--mi-normal-foreground-color);
+          font-family: var(--mi-font-family-stack);
+          -webkit-font-smoothing: auto;
+          -moz-osx-font-smoothing: auto;}
+        
+        body,
+        #root {
+          min-height: 100%;
+        }
+        
+        h1 {
+          font-size: 2.5rem;
+          line-height: 2;
+        
+          a {
+            text-decoration: none;
+            color: black;
+          }
+        }
+        
+        h2 {
+          font-size: 1.9rem;
+          line-height: 1.5;
+          margin-bottom: 0.4rem;
+        }
+        
+        h3 {
+          font-size: 1.7rem;
+          padding-bottom: 1rem;
+        }
+        
+        h4 { 
+          font-size: 1.4rem;
+          padding-bottom: .75rem;
+        }
+        
+        h5 {
+          font-size: 1.2rem;
+          padding-bottom: .75rem;
+        }
+        
+        h6 {
+          font-size: 1.1rem;
+          padding-bottom: .75rem;
+        }
+        
+        @media (max-width: 800px) {
+          h1 {
+            font-size: 2rem;
+          }
+        
+          h2 {
+            font-size: 1.8rem;
+          }
+        }
+        
+        a {
+          transition: .25s color, .25s background-color;
+          color: var(--mi-primary-color);
+        }
+        
+        @media (prefers-color-scheme: dark) {
+          a {
+            color: #F27D98;
+          }
+        }
+        
+        button {
+          font-family: var(--mi-font-family-stack);
+        }
+
+        code {
+          font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+            monospace;
+        }
+        
+        @keyframes slide-down {
+          from {
+            opacity: 0;
+            transform: translateY(-3rem);
+          }
+          to {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+        
+        @keyframes slide-up {
+          from {
+            opacity: 0;
+            transform: translateY(3rem);
+          }
+          to {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+        
+        @keyframes spinning {
+          from {
+            transform: rotate(0deg);
+          }
+          to {
+            transform: rotate(360deg);
+          }
+        }
+      `} />
       <div
         className={css`
           display: flex;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import ErrorPage from "./components/ErrorPage";
