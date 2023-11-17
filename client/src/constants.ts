d57df91b5609e2c2e069632ef97b58c5bcb9d702
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2023-11-17 20:01:20 +0100

    Added some constants and changed existing ones for future fine tunning

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,6 +1,9 @@
 export const API_ROOT = process.env.REACT_APP_API_DOMAIN;
 
 export const bp = {
-  small: "430",
-  medium: "800",
+  small: "576",
+  medium: "768",
+  large: "992",
+  xl: "1200",
+  xxl: "1400",
 };
