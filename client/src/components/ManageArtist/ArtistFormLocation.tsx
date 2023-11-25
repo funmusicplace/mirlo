5cb3e38dec00de16bd0a5899e9f0986f56efd3e9
Si <simsamsomred@gmail.com>
2023-11-25 06:49:50 -0500

    feat: add artist location

---
+++ client/src/components/ManageArtist/ArtistFormLocation.tsx
@@ -0,0 +1,121 @@
+import { useTranslation } from "react-i18next";
+import { useFieldArray, useForm } from "react-hook-form";
+import { InputEl } from "components/common/Input";
+import Button from "components/common/Button";
+import { css } from "@emotion/css";
+import { FaMapPin, FaPlus, FaSave, FaTimes, FaTrash } from "react-icons/fa";
+import React from "react";
+import LinkIconDisplay from "components/common/LinkIconDisplay";
+import api from "services/api";
+import { useArtistContext } from "state/ArtistContext";
+import { useGlobalStateContext } from "state/GlobalState";
+import { useSnackbar } from "state/SnackbarContext";
+
+interface FormData {
+  location: string;
+}
+
+const ArtistFormLocation: React.FC<{ isManage: boolean }> = ({ isManage }) => {
+  const [isEditing, setIsEditing] = React.useState(false);
+  const {
+    state: { artist },
+    refresh,
+  } = useArtistContext();
+  const {
+    state: { user },
+  } = useGlobalStateContext();
+  const artistId = artist?.id;
+  const artistUserId = artist?.userId;
+  const userId = user?.id;
+  const snackbar = useSnackbar();
+  const { t } = useTranslation("translation", { keyPrefix: "artist" });
+  const { register, handleSubmit, reset } = useForm<FormData>({
+    defaultValues: { location: artist?.location },
+  });
+
+  const doSave = React.useCallback(
+    async (data: FormData) => {
+      try {
+        if (userId && artistId && artistUserId === userId) {
+          await api.put(`users/${userId}/artists/${artistId}`, {
+            location: data.location,
+          });
+          refresh();
+          snackbar("Updated links", { type: "success" });
+        }
+      } catch (e) {
+      } finally {
+        setIsEditing(false);
+      }
+    },
+    [artistId, artistUserId, refresh, snackbar, userId]
+  );
+
+  if (!isEditing) {
+    return (
+      <div
+        className={css`
+          margin-bottom: 0.5rem;
+        `}
+      >
+        {artist?.location && (
+          <div
+            className={css`
+              svg {
+                margin-right: 0.5rem;
+              }
+            `}
+          >
+            <FaMapPin />
+            {artist?.location}
+          </div>
+        )}
+        {isManage && (
+          <Button compact onClick={() => setIsEditing(true)}>
+            {t("editLocation")}
+          </Button>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div
+        className={css`
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          max-width: 300px;
+          margin-bottom: 0.5rem;
+        `}
+      >
+        <FaMapPin />
+        <InputEl {...register(`location`)} placeholder="eg. North Pole" />
+      </div>
+      <div
+        className={css`
+          button {
+            margin-right: 0.5rem;
+          }
+        `}
+      >
+        <Button compact startIcon={<FaSave />} onClick={handleSubmit(doSave)}>
+          {t("saveLocation")}
+        </Button>
+        <Button
+          compact
+          startIcon={<FaTimes />}
+          onClick={() => {
+            reset();
+            setIsEditing(false);
+          }}
+        >
+          {t("cancel")}
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default ArtistFormLocation;
