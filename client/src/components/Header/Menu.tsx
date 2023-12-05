737c376cfbc2d2ac9a67690c73aa9ca49e0deeef
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2023-12-05 17:37:06 +0100

    clearer separation between "artists" and the rest

--- client/src/components/Header/Menu.tsx
+++ client/src/components/Header/Menu.tsx
@@ -119,7 +119,11 @@ const Menu: React.FC = (props) => {
               {t("collection")}
             </Button>
           </li>
-          <li>
+          <li
+            className={css`
+              border-bottom: solid 1px var(--mi-lighter-foreground-color);
+            `}
+          >
             <Button
               onClick={() => {
                 setIsMenuOpen(false);
@@ -151,7 +155,11 @@ const Menu: React.FC = (props) => {
               </li>
             );
           })}
-          <li>
+          <li
+            className={css`
+              border-top: solid 1px var(--mi-lighter-foreground-color);
+            `}
+          >
             <Button onClick={onLogOut} className={css``}>
               {t("logOut")}
             </Button>
@@ -178,7 +186,7 @@ const Menu: React.FC = (props) => {
       <li
         onClick={() => {
           setIsMenuOpen(false);
-          navigate("/about");
+          navigate("/pages/about");
         }}
       >
         <Button>{t("about")}</Button>
