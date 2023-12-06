fe64a8bf038309cd4e47e88a5b2a2f4265685aa8
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2023-12-06 19:05:13 +0100

    Removed unused import

--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -1,4 +1,3 @@
-import { css } from "@emotion/css";
 import { Trans, useTranslation } from "react-i18next";
 import { pageMarkdownWrapper } from "components/Post/index";
 import { MetaCard } from "../common/MetaCard";
