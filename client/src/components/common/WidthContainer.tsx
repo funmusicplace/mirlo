b6c103e48c66db9f9715e81ff774b6125047cb32
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2023-12-11 22:52:14 +0100

    created width container component

---
+++ client/src/components/common/WidthContainer.tsx
@@ -0,0 +1,58 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/css";
+
+type Sizes = "full" | "big" | "medium" | "small";
+
+const FlexWrapper = styled.div`
+  display: flex;
+`;
+
+const WidthWrapper = styled.div<{
+  variant?: Sizes;
+}>`
+  ${(props) => {
+    switch (props.variant) {
+      case "full":
+        return `
+        max-width: var(--mi-container-full-width);
+        `;
+      case "big":
+        return `
+        max-width: var(--mi-container-big);
+        `;
+      case "medium":
+        return `
+        max-width: var(--mi-container-medium);
+        `;
+      case "small":
+        return `
+        max-width: var(--mi-container-small);
+        `;
+      default:
+        return `
+        max-width: var(--mi-container-big);
+        `;
+    }
+  }}
+
+  width: 100%;
+`;
+
+export const WidthContainer: React.FC<{
+  children: React.ReactNode;
+  variant: Sizes;
+  justify: string;
+}> = ({ children, variant, justify }) => {
+  return (
+    <FlexWrapper
+      className={css`
+        justify-content: ${justify};
+        }
+      `}
+    >
+      <WidthWrapper variant={variant}>{children}</WidthWrapper>
+    </FlexWrapper>
+  );
+};
+
+export default WidthContainer;
