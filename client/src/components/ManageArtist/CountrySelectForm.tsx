e287eb5ab1fb26dd66dfc788e9cb2647a9b6be2d
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2023-12-12 18:54:05 +0100

    Aligned button

--- client/src/components/ManageArtist/CountrySelectForm.tsx
+++ client/src/components/ManageArtist/CountrySelectForm.tsx
@@ -45,17 +45,32 @@ const CountrySelect = () => {
     <form onSubmit={handleSubmit(doSave)}>
       <FormComponent
         className={css`
+          display: flex;
           * {
             margin-top: 0.5rem;
           }
           button {
-            height: 2.4rem;
-            margin-left: 0.5rem;
+            height: 100%;
+            margin: 0 0 0 0.5rem;
+          }
+          select {
+            margin: 0 !important;
           }
         `}
       >
         <label>{t("countryCurrencyCode")} </label>
-        <div>
+        <div
+          className={css`
+            display: flex;
+            align-items: flex-end;
+            button {
+              margin: 0 0 0 0.5rem;
+            }
+            select {
+              margin: 0 !important;
+            }
+          `}
+        >
           <SelectEl {...register("country")}>
             {stripeCountryCodes.map((cc) => (
               <option value={cc} key={cc}>
