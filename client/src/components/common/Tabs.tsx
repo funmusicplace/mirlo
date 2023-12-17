7e80056c6f9055f71a8178ba86d17de6a7a5e846
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2023-12-17 18:42:58 +0100

    padding adjustemnt to match hompage style

--- client/src/components/common/Tabs.tsx
+++ client/src/components/common/Tabs.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 const Tabs = styled.ul`
   list-style: none;
-  margin: 0.75rem 0.5rem 0 0;
+  margin: 1.1rem 0.5rem 0 0;
 
   > li {
     display: inline-block;
@@ -10,7 +10,8 @@ const Tabs = styled.ul`
     a {
       color: ${(props) => props.theme.colors.text};
       text-decoration: none;
-      padding: 0 0.5rem calc(0.75rem - 4px) 0;
+      padding: 0rem 0.5rem calc(0.9rem - 2px) 0;
+      line-height: 1rem;
       display: block;
       font-size: 1.2rem;
       transition: 0.1s border-bottom;
