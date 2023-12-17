e10a5ad31e98a11e6b27f176de02b689b7d40730
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2023-12-17 19:26:11 +0100

    adding padding to top for breathing space

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -236,96 +236,102 @@ function Home() {
           </div>
         </div>
       )}
-      {user && <Releases />}
-      {user && posts.length > 0 && (
-        <>
-          <div
-            className={css`
-              padding-top: 1rem;
-
-              background-color: var(--mi-light-background-color);
-            `}
-          >
-            <SectionHeader
-              className={css`
-                background-color: var(--mi-light-background-color) !important;
-              `}
-            >
-              <WidthContainer variant="big" justify="center">
-                <h5>{t("latestCommunityPost")}</h5>
-              </WidthContainer>
-            </SectionHeader>
-
+      <div
+        className={css`
+          padding-top: 1rem;
+        `}
+      >
+        {user && <Releases />}
+        {user && posts.length > 0 && (
+          <>
             <div
               className={css`
-                a {
-                  color: var(--mi-normal-foreground-color);
-                }
+                padding-top: 1rem;
+
+                background-color: var(--mi-light-background-color);
               `}
             >
-              <WidthContainer variant="big" justify="center">
-                <div
-                  className={css`
-                    margin: var(--mi-side-paddings-xsmall);
-                    margin-top: 1rem;
-                    display: flex;
-                    flex-wrap: wrap;
-                    justify-content: space-between;
+              <SectionHeader
+                className={css`
+                  background-color: var(--mi-light-background-color) !important;
+                `}
+              >
+                <WidthContainer variant="big" justify="center">
+                  <h5>{t("latestCommunityPost")}</h5>
+                </WidthContainer>
+              </SectionHeader>
 
-                    a {
-                      width: 32%;
-                    }
-
-                    @media screen and (max-width: ${bp.medium}px) {
-                      flex-direction: column;
+              <div
+                className={css`
+                  a {
+                    color: var(--mi-normal-foreground-color);
+                  }
+                `}
+              >
+                <WidthContainer variant="big" justify="center">
+                  <div
+                    className={css`
+                      margin: var(--mi-side-paddings-xsmall);
+                      margin-top: 1rem;
+                      display: flex;
+                      flex-wrap: wrap;
+                      justify-content: space-between;
 
                       a {
-                        width: 100%;
+                        width: 32%;
                       }
-                    }
-                  `}
-                >
-                  {posts.map((p) => (
-                    <Link
-                      to={`/post/${p.id}/`}
-                      className={css`
-                        display: flex;
-                        margin-bottom: 1.5rem;
-                        border-radius: 5px;
-                        background-color: var(--mi-darken-background-color);
-                        filter: brightness(95%);
-                        width: 100%;
 
-                        :hover {
-                          transition: 0.2s ease-in-out;
-                          text-decoration: none;
-                          background-color: rgba(50, 0, 0, 0.07);
-                          filter: brightness(90%);
+                      @media screen and (max-width: ${bp.medium}px) {
+                        flex-direction: column;
+
+                        a {
+                          width: 100%;
                         }
+                      }
+                    `}
+                  >
+                    {posts.map((p) => (
+                      <Link
+                        to={`/post/${p.id}/`}
+                        className={css`
+                          display: flex;
+                          margin-bottom: 1.5rem;
+                          border-radius: 5px;
+                          background-color: var(--mi-darken-background-color);
+                          filter: brightness(95%);
+                          width: 100%;
 
-                        @media (prefers-color-scheme: dark) {
                           :hover {
-                            filter: brightness(120%);
-                            background-color: rgba(100, 100, 100, 0.2);
+                            transition: 0.2s ease-in-out;
+                            text-decoration: none;
+                            background-color: rgba(50, 0, 0, 0.07);
+                            filter: brightness(90%);
                           }
-                        }
-                      `}
-                    >
-                      <Overlay width="100%" height="100%"></Overlay>
-                      <PostCard
-                        width="100%"
-                        height="350px"
-                        dateposition="100%"
-                        p={p}
-                      ></PostCard>
-                    </Link>
-                  ))}
-                </div>
-              </WidthContainer>
+
+                          @media (prefers-color-scheme: dark) {
+                            :hover {
+                              filter: brightness(120%);
+                              background-color: rgba(100, 100, 100, 0.2);
+                            }
+                          }
+                        `}
+                      >
+                        <Overlay width="100%" height="100%"></Overlay>
+                        <PostCard
+                          width="100%"
+                          height="350px"
+                          dateposition="100%"
+                          p={p}
+                        ></PostCard>
+                      </Link>
+                    ))}
+                  </div>
+                </WidthContainer>
+              </div>
             </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
