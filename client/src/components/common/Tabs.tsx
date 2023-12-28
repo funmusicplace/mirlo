e2cc7f96400f49e270ef6061264b672529f4f6cc
Si <simsamsomred@gmail.com>
2023-12-28 16:37:31 -0500

    fix: better styles for tabs on artist page

--- client/src/components/common/Tabs.tsx
+++ client/src/components/common/Tabs.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { bp } from "../../constants";
 
 const Tabs = styled.ul`
   list-style: none;
@@ -11,12 +12,16 @@ const Tabs = styled.ul`
 
     a {
       text-decoration: none;
-      padding: 0rem 0.5rem calc(0.9rem - 2px) 0.25rem;
+      padding: 0rem 0.25rem calc(0.9rem - 4px) 0.25rem;
       line-height: 1rem;
       display: block;
       font-size: 1.2rem;
       transition: 0.1s border-bottom;
       border-bottom: 4px solid transparent;
+
+      @media screen and (max-width: ${bp.medium}px) {
+        padding: 0rem 0.25rem calc(0.9rem - 4px) 0.25rem;
+      }
 
       &.active {
         border-bottom: 4px solid var(--mi-primary-color);
@@ -26,6 +31,13 @@ const Tabs = styled.ul`
         border-bottom: 4px solid var(--mi-primary-color);
       }
     }
+  }
+`;
+
+export const ArtistTabs = styled(Tabs)`
+  @media screen and (max-width: ${bp.medium}px) {
+    padding: var(--mi-side-paddings-xsmall);
+    margin-bottom: 0.5rem;
   }
 `;
 
