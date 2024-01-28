5c3785742d89dd592d2201c3a72bfdb84fd9848a
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2024-01-28 13:37:52 +0100

    Made pill always have border or correct background

--- client/src/components/common/Pill.tsx
+++ client/src/components/common/Pill.tsx
@@ -4,6 +4,7 @@ const Pill = styled.span`
   display: inline-flex;
   background-color: var(--mi-darken-x-background-color);
   border-radius: var(--mi-border-radius-x);
+  border: var(--mi-border);
   padding: 0.25rem 0.5rem;
   font-size: 0.9rem;
   margin: 0 0.3rem;
@@ -12,6 +13,9 @@ const Pill = styled.span`
   button {
     margin-left: 0.25rem;
     padding: 0;
+  }
+  @media (prefers-color-scheme: dark) {
+    background-color: var(--mi-lighten-background-color);
   }
 `;
 
