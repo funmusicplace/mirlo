11a660a1f930c217267602124aeb79b7da866e3a
Simon <simsamsomred@gmail.com>
2024-05-07 15:45:39 -0400

    fix: run notifications email sequentially

--- src/jobs/send-notification-email.ts
+++ src/jobs/send-notification-email.ts
@@ -25,51 +25,49 @@ const sendNotificationEmail = async () => {
     },
   });
 
-  await Promise.all(
-    notifications.map(async (notification) => {
-      if (
-        notification.notificationType === "NEW_ARTIST_POST" &&
-        notification.post?.artist
-      ) {
-        if (!notification.post.shouldSendEmail) {
-          logger.info(
-            `sendNotificationEmail: post asked not to be emailed: ${notification.post.title} to ${notification.user.email}`
-          );
-          return;
-        } else {
-          logger.info(
-            `sendNotificationEmail: mailing notification for: ${notification.post.title} to ${notification.user.email}`
-          );
-          await sendMail({
-            data: {
-              template: "announce-post-published",
-              message: {
-                to: notification.user.email,
-              },
-              locals: {
-                artist: notification.post.artist,
-                post: {
-                  ...notification.post,
-                  htmlContent: notification.post.content,
-                },
-                email: notification.user.email,
-                host: process.env.API_DOMAIN,
-                client: process.env.REACT_APP_CLIENT_DOMAIN,
-              },
-            },
-          });
-        }
-        await prisma.notification.update({
-          where: {
-            id: notification.id,
-          },
+  for (const notification of notifications) {
+    if (
+      notification.notificationType === "NEW_ARTIST_POST" &&
+      notification.post?.artist
+    ) {
+      if (!notification.post.shouldSendEmail) {
+        logger.info(
+          `sendNotificationEmail: post asked not to be emailed: ${notification.post.title} to ${notification.user.email}`
+        );
+        return;
+      } else {
+        logger.info(
+          `sendNotificationEmail: mailing notification for: ${notification.post.title} to ${notification.user.email}`
+        );
+        await sendMail({
           data: {
-            isRead: true,
+            template: "announce-post-published",
+            message: {
+              to: notification.user.email,
+            },
+            locals: {
+              artist: notification.post.artist,
+              post: {
+                ...notification.post,
+                htmlContent: notification.post.content,
+              },
+              email: notification.user.email,
+              host: process.env.API_DOMAIN,
+              client: process.env.REACT_APP_CLIENT_DOMAIN,
+            },
           },
         });
       }
-    })
-  );
+      await prisma.notification.update({
+        where: {
+          id: notification.id,
+        },
+        data: {
+          isRead: true,
+        },
+      });
+    }
+  }
 };
 
 export default sendNotificationEmail;
