3b1fc2a9f6070124e8ce7822314e9d8863158499
Simon <simsamsomred@gmail.com>
2024-05-07 15:59:03 -0400

    fix: add error wrappers to notifications posts

--- src/jobs/send-notification-email.ts
+++ src/jobs/send-notification-email.ts
@@ -25,48 +25,60 @@ const sendNotificationEmail = async () => {
     },
   });
 
-  for await (const notification of notifications) {
-    if (
-      notification.notificationType === "NEW_ARTIST_POST" &&
-      notification.post?.artist
-    ) {
-      if (!notification.post.shouldSendEmail) {
-        logger.info(
-          `sendNotificationEmail: post asked not to be emailed: ${notification.post.title} to ${notification.user.email}`
-        );
-        return;
-      } else {
-        logger.info(
-          `sendNotificationEmail: mailing notification for: ${notification.post.title} to ${notification.user.email}`
-        );
-        await sendMail({
-          data: {
-            template: "announce-post-published",
-            message: {
-              to: notification.user.email,
-            },
-            locals: {
-              artist: notification.post.artist,
-              post: {
-                ...notification.post,
-                htmlContent: notification.post.content,
+  try {
+    for await (const notification of notifications) {
+      if (
+        notification.notificationType === "NEW_ARTIST_POST" &&
+        notification.post?.artist
+      ) {
+        if (!notification.post.shouldSendEmail) {
+          logger.info(
+            `sendNotificationEmail: post asked not to be emailed: ${notification.post.title} to ${notification.user.email}`
+          );
+          return;
+        } else {
+          logger.info(
+            `sendNotificationEmail: mailing notification for: ${notification.post.title} to ${notification.user.email}`
+          );
+          try {
+            await sendMail({
+              data: {
+                template: "announce-post-published",
+                message: {
+                  to: notification.user.email,
+                },
+                locals: {
+                  artist: notification.post.artist,
+                  post: {
+                    ...notification.post,
+                    htmlContent: notification.post.content,
+                  },
+                  email: notification.user.email,
+                  host: process.env.API_DOMAIN,
+                  client: process.env.REACT_APP_CLIENT_DOMAIN,
+                },
               },
-              email: notification.user.email,
-              host: process.env.API_DOMAIN,
-              client: process.env.REACT_APP_CLIENT_DOMAIN,
-            },
+            });
+          } catch (e) {
+            logger.error(
+              `failed to send e-mail of notification ${notification.id} to ${notification.user.email}`
+            );
+            logger.error(e);
+          }
+        }
+        await prisma.notification.update({
+          where: {
+            id: notification.id,
+          },
+          data: {
+            isRead: true,
           },
         });
       }
-      await prisma.notification.update({
-        where: {
-          id: notification.id,
-        },
-        data: {
-          isRead: true,
-        },
-      });
     }
+  } catch (e) {
+    logger.error(`failed to send out all notifications`);
+    logger.error(e);
   }
 };
 
