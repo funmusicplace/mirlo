7b78c8f02e0258376ab0534dd683c6aa71d19f3a
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2024-05-12 02:45:35 +0200

    Update UploadArtistImage.tsx
    Camelcase correction

--- client/src/components/ManageArtist/UploadArtistImage.tsx
+++ client/src/components/ManageArtist/UploadArtistImage.tsx
@@ -54,14 +54,14 @@ const UploadArtistImage: React.FC<{
   height: string;
   width: string;
   maxDimensions: string;
-  imagetypedescription: string;
+  imageTypeDescription: string;
 }> = ({
   existing,
   imageType,
   height,
   width,
   maxDimensions,
-  imagetypedescription,
+  imageTypeDescription,
 }) => {
   const { t } = useTranslation("translation", { keyPrefix: "artistForm" });
   const snackbar = useSnackbar();
@@ -170,7 +170,7 @@ const UploadArtistImage: React.FC<{
                 width={width}
                 height={height}
                 rounded={rounded}
-                imagetypedescription={imagetypedescription}
+                imageTypeDescription={imageTypeDescription}
               />
             )}
             {(isLoading || isSaving) && <Spinner rounded={rounded} />}
