abd809ad7417fccaf8499af87f17f0d545938bb6
human curation <humancurationcoop@pm.me>
2024-10-05 22:51:18 -0700

    api fixed

--- test/api/api.test.ts
+++ test/api/api.test.ts
@@ -1,1 +1,21 @@
 import sinon from 'sinon';
+import { expect } from 'chai';
+import api from '../../api';
+import APIInstance from '../../APIInstance';
+
+describe('API Tests', () => {
+  let APIInstanceMock: sinon.SinonStub;
+
+  before(() => {
+    APIInstanceMock = sinon.stub(APIInstance, 'default').returns('mocked API instance');
+  });
+
+  after(() => {
+    APIInstanceMock.restore();
+  });
+
+  it('should instantiate API with correct root URL', () => {
+    expect(api).to.equal('mocked API instance');
+    sinon.assert.calledWith(APIInstanceMock, sinon.match.string);
+  });
+});
