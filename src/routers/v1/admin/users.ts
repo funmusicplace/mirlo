f096ae67ce2aa842bfbdf02bb9dc3641bb5af5f7
Simon <simsamsomred@gmail.com>
2024-12-10 17:05:12 -0500

    fix: missing users endoint

---
+++ src/routers/v1/admin/users.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import prisma from "@mirlo/prisma";
+import { userAuthenticated, userHasPermission } from "../../../auth/passport";
+import { uniqBy } from "lodash";
+import logger from "../../../logger";
+
+export default function () {
+  const operations = {
+    POST: [userAuthenticated, userHasPermission("admin"), POST],
+  };
+
+  async function POST(req: Request, res: Response, next: NextFunction) {
+    const { users } = req.body as { users: { email: string }[] };
+    try {
+      const existingEmails = (
+        await prisma.user.findMany({
+          where: { email: { in: users.map((s) => s.email) } },
+          select: { email: true },
+        })
+      ).map((user) => user.email);
+      const newUsers = users.filter(
+        (user) => !existingEmails.includes(user.email)
+      );
+      await prisma.user.createMany({
+        data: uniqBy(newUsers, "email").map((newUser) => newUser),
+      });
+      res.json({
+        message: "success",
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  POST.apiDoc = {
+    summary: "Creates users based on emails supplied",
+    responses: {
+      default: {
+        description: "An error occurred",
+        schema: {
+          additionalProperties: true,
+        },
+      },
+    },
+  };
+
+  return operations;
+}
