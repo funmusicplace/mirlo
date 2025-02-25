6520d268f89b8b2af04a1986200cc56aa2a533c9
Leni Kadali <52788034+lenikadali@users.noreply.github.com>
2025-02-25 22:34:35 +0300

    Add tip query, use it in Tip/CheckoutComplete
    Added a query for tips and (have tried to) use it
    in Tip/CheckoutComplete. Will need more work
    to make it conform to the types and overall conventions.

--- client/src/components/Tip/CheckoutComplete.tsx
+++ client/src/components/Tip/CheckoutComplete.tsx
@@ -9,30 +9,24 @@ import ImageWithPlaceholder from "components/common/ImageWithPlaceholder";
 import { WidthWrapper } from "components/common/WidthContainer";
 
 import { useQuery } from "@tanstack/react-query";
-import { queryArtist, queryMerch } from "queries";
+import { queryArtist, queryTip } from "queries";
 
 function TrackGroup() {
   const { t } = useTranslation("translation", {
     keyPrefix: "merchDetails",
   });
 
-  const { artistId, merchId } = useParams();
+  const { artistId, artistSlug, tipId } = useParams();
   const { data: artist, isLoading: isLoadingArtist } = useQuery(
     queryArtist({ artistSlug: artistId ?? "" })
   );
-  const { data: merch, isLoading: isLoadingMerch } = useQuery(
-    queryMerch({ merchId: merchId ?? "" })
+  const { data: tip, isLoading: isLoadingTip } = useQuery(
+    queryTip({ artistId: artist.id, artistSlug: artistSlug, tipId: tipId })
   );
 
   if (!artist && !isLoadingArtist) {
     return <Box>{t("doesNotExist")}</Box>;
   } else if (!artist) {
-    return <FullPageLoadingSpinner />;
-  }
-
-  if (!merch && !isLoadingMerch) {
-    return <Box>{t("doesNotExist")}</Box>;
-  } else if (!merch) {
     return <FullPageLoadingSpinner />;
   }
 
@@ -54,7 +48,7 @@ function TrackGroup() {
         }
       `}
     >
-      <h1>Purchase complete!</h1>
+      <h1>You have successfully tipped this artist!</h1>
       <div
         className={css`
           display: flex;
@@ -65,8 +59,8 @@ function TrackGroup() {
         `}
       >
         <ImageWithPlaceholder
-          src={merch.images?.[0]?.sizes?.[120]}
-          alt={merch.title}
+          src={artist.avatar?.[0]?.sizes?.[120]}
+          alt={artist.name}
           size={120}
         />
         <div
@@ -75,7 +69,7 @@ function TrackGroup() {
           `}
         >
           {" "}
-          You've bought {merch.title}! The artist has been notified.
+          You've tipped {artist.name}! The artist has been notified.
         </div>
       </div>
       <Confetti />
