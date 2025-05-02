32b358eefc2e54caaae0dc2c3a7059755becf9c0
Simon <simsamsomred@gmail.com>
2025-05-02 17:01:06 -0400

    fix: fix the checkout complete url for track purchase

--- src/routers/v1/checkout/index.ts
+++ src/routers/v1/checkout/index.ts
@@ -91,11 +91,15 @@ export default function () {
             // checkout callbackURL separately from the applicationURL
             // and that callbackURL should probably contain a pattern that
             // they can define
+            const track = await prisma.track.findFirst({
+              where: { id: +trackId },
+              include: {
+                trackGroup: true,
+              },
+            });
             res.redirect(
               client?.applicationUrl +
-                `/${artist?.urlSlug}?trackPurchase=${
-                  success ? "success" : "canceled"
-                }&trackId=${trackId}`
+                `/${artist?.urlSlug}/release/${track?.trackGroup.urlSlug}/tracks/${trackId}/checkout-complete`
             );
           } else {
             res.status(500).json({
