35aac32d186841805f3c5e496bde496cbe9f7238
Simon <simsamsomred@gmail.com>
2025-06-28 10:32:46 -0400

    fix: handle urlSlug for posts

--- src/parseIndex.ts
+++ src/parseIndex.ts
@@ -12,6 +12,7 @@ import {
   finalUserAvatarBucket,
 } from "./utils/minio";
 import { Client } from "@mirlo/prisma/client";
+import { isNumber } from "lodash";
 
 type Options = {
   title: string;
@@ -158,18 +159,35 @@ const parseIndex = async (pathname: string) => {
         : undefined;
       const rss = `${process.env.API_DOMAIN}/v1/artists/${artist.urlSlug}/feed?format=rss`;
       if (route[2] === "posts") {
-        const post = await prisma.post.findFirst({
-          where: { id: Number(route[3]) },
-          include: {
-            artist: true,
-            featuredImage: true,
-            tracks: {
-              orderBy: {
-                order: "asc",
+        let post;
+        if (isNumber(route[3])) {
+          post = await prisma.post.findFirst({
+            where: { id: Number(route[3]) },
+            include: {
+              artist: true,
+              featuredImage: true,
+              tracks: {
+                orderBy: {
+                  order: "asc",
+                },
               },
             },
-          },
-        });
+          });
+        } else if (route[3]) {
+          post = await prisma.post.findFirst({
+            where: { urlSlug: route[3] },
+            include: {
+              artist: true,
+              featuredImage: true,
+              tracks: {
+                orderBy: {
+                  order: "asc",
+                },
+              },
+            },
+          });
+        }
+
         if (post) {
           const hasTracks = post.tracks.length > 0;
           // it's a post
