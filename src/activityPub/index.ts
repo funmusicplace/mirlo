16ab40550e1c358c20f362c6cedbf73152632ae7
Simon <simsamsomred@gmail.com>
2025-07-18 09:31:59 -0400

    fix: assetlinks returns []
    closes #1341

--- src/activityPub/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import webfinger from "./webfinger";
-
-const router = express.Router();
-
-router.get("/.well-known/webfinger", webfinger);
-
-export default router;
