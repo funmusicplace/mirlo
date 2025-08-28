c6c91669ef1544749aff92a3ac76bc809b013c9d
Simon <simsamsomred@gmail.com>
2025-08-28 09:29:00 -0400

    fix: move currency update to update account webhook

--- src/routers/v1/users/{userId}/stripe/connectComplete.ts
+++ src/routers/v1/users/{userId}/stripe/connectComplete.ts
@@ -1,14 +1,6 @@
-import { User } from "@mirlo/prisma/client";
 import { Request, Response } from "express";
-import { userAuthenticated } from "../../../../../auth/passport";
-import prisma from "@mirlo/prisma";
 
-import stripe from "../../../../../utils/stripe";
-import logger from "../../../../../logger";
 import { getClient } from "../../../../../activityPub/utils";
-import { AppError } from "../../../../../utils/error";
-import { updateCurrencies } from "../../../../../utils/user";
-const { API_DOMAIN } = process.env;
 
 type Params = {
   userId: string;
@@ -20,45 +12,10 @@ export default function () {
   };
 
   async function GET(req: Request, res: Response) {
-    const { userId } = req.params as unknown as Params;
-    const loggedInUser = req.user as User;
-
     try {
-      if (Number(userId) === Number(loggedInUser.id)) {
-        const user = await prisma.user.findUnique({
-          where: { id: Number(userId) },
-        });
-        if (user) {
-          let accountId = user.stripeAccountId;
+      const client = await getClient();
 
-          if (!accountId) {
-            throw new AppError({
-              httpCode: 400,
-              description: "No Stripe account found",
-            });
-          }
-          const client = await getClient();
-
-          logger.info(
-            `Connecting ${user.id} to Stripe. Have existing account:  ${accountId}`
-          );
-
-          try {
-            const stripeAccount = await stripe.accounts.retrieve(accountId);
-            if (stripeAccount.default_currency) {
-              updateCurrencies(user.id, stripeAccount.default_currency);
-            }
-          } catch (e) {
-            console.error(`Error retrieving account information about user`, e);
-          }
-
-          res.redirect(`${client?.applicationUrl}/manage?stripeConnect=done`);
-        }
-      } else {
-        res.status(401).json({
-          error: "Invalid route",
-        });
-      }
+      res.redirect(`${client?.applicationUrl}/manage?stripeConnect=done`);
     } catch (e) {
       console.error(e);
       res.json({
