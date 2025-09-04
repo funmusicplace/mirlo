9c62e386e5faaf224a39060f9812d48877b107fc
Simon <simsamsomred@gmail.com>
2025-09-04 09:52:21 -0400

    fixed artist buttons at the bottom

--- client/src/components/common/FixedButton.tsx
+++ client/src/components/common/FixedButton.tsx
@@ -3,11 +3,13 @@ import { ButtonLink } from "./Button";
 import { bp } from "../../constants";
 
 const FixedButtonLink = styled(ButtonLink)`
-  box-shadow: 0.2rem 0.2rem 0.3rem rgba(0, 0, 0, 0.5);
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
   background-color: rgba(255, 255, 255, 0.9) !important;
   color: black !important;
+  height: auto;
+  width: auto;
 
-  justify-content: left;
+  justify-content: space-between;
 
   :hover {
     background-color: rgba(0, 0, 0, 0.7) !important;
@@ -16,6 +18,10 @@ const FixedButtonLink = styled(ButtonLink)`
 
   @media screen and (min-width: ${bp.medium}px) {
     padding: 0.5rem !important;
+  }
+
+  .endIcon {
+    font-size: 1.25rem;
   }
 `;
 
