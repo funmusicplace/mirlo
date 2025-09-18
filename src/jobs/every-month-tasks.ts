50800791c1408a5703fae7bc0200ea326d0269e8
Simon <simsamsomred@gmail.com>
2025-09-18 18:02:59 -0400

    fix: add monthly report email

--- src/jobs/every-month-tasks.ts
+++ src/jobs/every-month-tasks.ts
@@ -2,9 +2,11 @@
 import cleanUpTrackGroups from "./clean-up-trackgroups";
 import cleanUpUserAccounts from "./clean-up-user-accounts";
 import sendOutMonthlyReceipts from "./send-out-monthly-receipts";
+import sendOutMonthlyIncomeReport from "./send-out-monthy-income-report";
 
 const triggerMonthlyTasks = async () => {
   await sendOutMonthlyReceipts();
+  await sendOutMonthlyIncomeReport();
   await cleanUpTrackGroups();
   await cleanUpUserAccounts();
   // await cleanUpFiles();
