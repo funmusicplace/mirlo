5fc164379e1f230969b8390efb0784cc3458b4d6
Simon <simsamsomred@gmail.com>
2025-09-19 13:56:32 -0400

    fix: allow shipping to EU as one rate

--- client/src/components/Merch/BuyMerchItem.tsx
+++ client/src/components/Merch/BuyMerchItem.tsx
@@ -6,7 +6,7 @@ import { bp } from "../../constants";
 
 import Button from "components/common/Button";
 import { InputEl } from "components/common/Input";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import FormComponent from "components/common/FormComponent";
 import React from "react";
 import { css } from "@emotion/css";
@@ -20,8 +20,9 @@ import { flatten } from "lodash";
 import Box from "components/common/Box";
 import TextArea from "components/common/TextArea";
 import EmbeddedStripeForm from "components/common/stripe/EmbeddedStripe";
+import BuyMerchItemDestinations from "./BuyMerchItemDestinations";
 
-type FormData = {
+export type BuyMerchFormData = {
   quantity: number;
   price: number;
   merchOptionIds: string[];
@@ -53,7 +54,7 @@ const BuyMerchItem: React.FC<{
 
   const minPrice = (merch?.minPrice ?? 0) / 100;
 
-  const methods = useForm<FormData>({
+  const methods = useForm<BuyMerchFormData>({
     defaultValues: {
       price: minPrice,
       quantity: 1,
@@ -74,7 +75,7 @@ const BuyMerchItem: React.FC<{
   }, [quantity, minPrice, setValue]);
 
   const onSubmit = React.useCallback(
-    async (data: FormData) => {
+    async (data: BuyMerchFormData) => {
       try {
         if (merch) {
           const options = flatten(
@@ -138,16 +139,6 @@ const BuyMerchItem: React.FC<{
   let price = Number(currentPrice) * Number(quantity);
   let amountAvailable = merch.quantityRemaining;
 
-  merch.shippingDestinations.forEach((sd) => {
-    if (sd.id === shippingDestination) {
-      const otherUnitCount = Number(quantity) - 1;
-      const costUnit = sd.costUnit / 100;
-      const costExtraUnit = (sd.costExtraUnit * otherUnitCount) / 100;
-
-      price += costUnit + costExtraUnit;
-    }
-  });
-
   merch.optionTypes?.forEach((ot) => {
     ot.options.forEach((o) => {
       if (merchOptionIds.includes(o.id)) {
@@ -159,12 +150,17 @@ const BuyMerchItem: React.FC<{
     });
   });
 
-  const exceedsAvailable = amountAvailable < Number(quantity);
+  merch.shippingDestinations.forEach((sd) => {
+    if (sd.id === shippingDestination) {
+      const otherUnitCount = Number(quantity) - 1;
+      const costUnit = sd.costUnit / 100;
+      const costExtraUnit = (sd.costExtraUnit * otherUnitCount) / 100;
 
-  const onlyOneDestination = merch.shippingDestinations.length === 1;
-  const defaultOption = onlyOneDestination
-    ? t("everywhere")
-    : t("everywhereElse");
+      price += costUnit + costExtraUnit;
+    }
+  });
+
+  const exceedsAvailable = amountAvailable < Number(quantity);
 
   if (clientSecret && stripeAccountStatus?.stripeAccountId) {
     return (
@@ -175,232 +171,194 @@ const BuyMerchItem: React.FC<{
     );
   }
 
-  const currentDestination = merch.shippingDestinations.find(
-    (sd) => sd.id === shippingDestination
-  );
-
   return (
-    <form
-      onSubmit={methods.handleSubmit(onSubmit)}
-      className={css`
-        padding: 1rem;
-        margin-top: -1rem;
-      `}
-    >
-      <div
+    <FormProvider {...methods}>
+      <form
+        onSubmit={methods.handleSubmit(onSubmit)}
         className={css`
-          @media screen and (min-width: ${bp.medium}px) {
-            display: flex;
-            flex-direction: row;
-          }
+          padding: 1rem;
+          margin-top: -1rem;
+        `}
+      >
+        <div
+          className={css`
+            @media screen and (min-width: ${bp.medium}px) {
+              display: flex;
+              flex-direction: row;
+            }
 
-          > div {
-            width: 49%;
-            margin-right: 1rem;
-          }
-        `}
-      >
-        <FormComponent>
-          <label htmlFor="quantity">{t("howMany")}</label>
-          <InputEl
-            {...methods.register("quantity", {
-              min: 1,
-              max: merch.quantityRemaining,
-            })}
-            id="quantity"
-            type="number"
-            min={1}
-            max={merch.quantityRemaining}
-          />
-          {formState.errors.quantity && (
-            <Box variant="warning" compact>
-              {formState.errors.quantity.message}
-            </Box>
-          )}
-          {exceedsAvailable && (
-            <Box variant="warning" compact>
-              {t("notEnoughInStockQuantity")}
-            </Box>
-          )}
-        </FormComponent>
-        <FormComponent>
-          <label htmlFor="price">
-            {t("howMuch", { currency: getCurrencySymbol(merch.currency) })}
-          </label>
-          <InputEl
-            {...methods.register("price", { min: minPrice })}
-            type="number"
-            id="price"
-            min={minPrice ? minPrice * quantity : 0}
-            step={0.01}
-          />
-          {price < minPrice && (
-            <Box variant="warning" compact>
-              {t("priceMustBeAtLeast", {
-                price: moneyDisplay({
-                  amount: minPrice,
-                  currency: merch.currency,
-                }),
-              })}
-            </Box>
-          )}
-        </FormComponent>
-      </div>
-      <div
-        className={css`
-          @media screen and (min-width: ${bp.medium}px) {
-            display: flex;
-            flex-direction: row;
-          }
-          padding-bottom: 1rem;
-          margin-bottom: 2rem;
-          border-bottom: 1px solid var(--mi-darken-x-background-color);
-          > div {
-            width: 49%;
-            margin-right: 1rem;
-          }
-        `}
-      >
-        {merch.optionTypes?.map((optionType, idx) => (
+            > div {
+              width: 49%;
+              margin-right: 1rem;
+            }
+          `}
+        >
           <FormComponent>
-            <label htmlFor={`merchOptionIds.${idx}`}>
-              {optionType.optionName}
-            </label>
-            <SelectEl
-              id={`merchOptionIds.${idx}`}
-              {...methods.register(`merchOptionIds.${idx}`)}
-              required
-            >
-              <option value="">{t("choose")}</option>
-
-              {optionType.options
-                .sort((a, b) => {
-                  return a.additionalPrice < b.additionalPrice ? -1 : 1;
-                })
-                .map((o) => (
-                  <option
-                    key={o.name}
-                    value={o.id}
-                    disabled={
-                      o.quantityRemaining !== null
-                        ? o.quantityRemaining < quantity
-                        : false
-                    }
-                  >
-                    {o.additionalPrice
-                      ? t("option", {
-                          name: o.name,
-                          costUnit: moneyDisplay({
-                            amount: o.additionalPrice / 100,
-                            currency: merch.currency,
-                          }),
-                        })
-                      : o.name}
-                  </option>
-                ))}
-            </SelectEl>
-            {formState.errors.merchOptionIds && (
-              <Box
-                variant="warning"
-                className={css`
-                  margin-top: 0.5rem;
-                `}
-                compact
-              >
-                {formState.errors.merchOptionIds.message}
+            <label htmlFor="quantity">{t("howMany")}</label>
+            <InputEl
+              {...methods.register("quantity", {
+                min: 1,
+                max: merch.quantityRemaining,
+              })}
+              id="quantity"
+              type="number"
+              min={1}
+              max={merch.quantityRemaining}
+            />
+            {formState.errors.quantity && (
+              <Box variant="warning" compact>
+                {formState.errors.quantity.message}
+              </Box>
+            )}
+            {exceedsAvailable && (
+              <Box variant="warning" compact>
+                {t("notEnoughInStockQuantity")}
               </Box>
             )}
           </FormComponent>
-        ))}
-      </div>
-      <FormComponent
-        className={css`
-          padding-bottom: 2rem;
-          margin-bottom: 2rem !important;
-          border-bottom: 1px solid var(--mi-darken-x-background-color);
-        `}
-      >
-        <label htmlFor="shippingDestinationId">
-          {t("supportedShippingDestinations")}
-        </label>
-        <SelectEl
-          id="shippingDestinationId"
-          {...methods.register(`shippingDestinationId`)}
-        >
-          {merch.shippingDestinations.map((o) => (
-            <option key={o.id} value={o.id}>
-              {o.destinationCountry && o.destinationCountry !== ""
-                ? `${codeToCountryMap[o.destinationCountry].countryName} (${o.destinationCountry})`
-                : defaultOption}{" "}
-            </option>
-          ))}
-        </SelectEl>
-        {currentDestination && (
-          <small>
-            {t("destinationCost", {
-              costUnit: moneyDisplay({
-                amount: currentDestination.costUnit / 100,
-                currency: currentDestination.currency,
-              }),
-              costExtraUnit: moneyDisplay({
-                amount: currentDestination.costExtraUnit / 100,
-                currency: currentDestination.currency,
-              }),
-            })}
-          </small>
-        )}
-      </FormComponent>
-      <div
-        className={css`
-          border-bottom: 1px solid var(--mi-darken-x-background-color);
-          padding-bottom: 1rem;
-          margin-bottom: 2rem;
-        `}
-      >
-        <p
+          <FormComponent>
+            <label htmlFor="price">
+              {t("howMuch", { currency: getCurrencySymbol(merch.currency) })}
+            </label>
+            <InputEl
+              {...methods.register("price", { min: minPrice })}
+              type="number"
+              id="price"
+              min={minPrice ? minPrice * quantity : 0}
+              step={0.01}
+            />
+            {price < minPrice && (
+              <Box variant="warning" compact>
+                {t("priceMustBeAtLeast", {
+                  price: moneyDisplay({
+                    amount: minPrice,
+                    currency: merch.currency,
+                  }),
+                })}
+              </Box>
+            )}
+          </FormComponent>
+        </div>
+        <div
           className={css`
-            margin: 1rem auto;
-            font-weight: bold;
+            @media screen and (min-width: ${bp.medium}px) {
+              display: flex;
+              flex-direction: row;
+            }
+            padding-bottom: 1rem;
+            margin-bottom: 2rem;
+            border-bottom: 1px solid var(--mi-darken-x-background-color);
+            > div {
+              width: 49%;
+              margin-right: 1rem;
+            }
           `}
         >
-          {t("orderTotal", {
-            amount: moneyDisplay({ amount: price, currency: merch.currency }),
-          })}
-        </p>
-        <IncludesDigitalDownload merch={merch} artist={artist} />
-      </div>
+          {merch.optionTypes?.map((optionType, idx) => (
+            <FormComponent>
+              <label htmlFor={`merchOptionIds.${idx}`}>
+                {optionType.optionName}
+              </label>
+              <SelectEl
+                id={`merchOptionIds.${idx}`}
+                {...methods.register(`merchOptionIds.${idx}`)}
+                required
+              >
+                <option value="">{t("choose")}</option>
 
-      <FormComponent>
-        <label htmlFor="comment">{t("leaveAComment")}</label>
-        <TextArea id="comment" {...methods.register("message")} rows={2} />
-        <small>{t("messageToArtist")}</small>
-      </FormComponent>
-
-      <div
-        className={css`
-          display: flex;
-          justify-content: flex-end;
-          margin-bottom: 2rem;
-        `}
-      >
-        <Button
-          disabled={!methods.formState.isValid || exceedsAvailable}
-          isLoading={isLoadingStripe}
-          size="big"
-          rounded
-          endIcon={<FaChevronRight />}
+                {optionType.options
+                  .sort((a, b) => {
+                    return a.additionalPrice < b.additionalPrice ? -1 : 1;
+                  })
+                  .map((o) => (
+                    <option
+                      key={o.name}
+                      value={o.id}
+                      disabled={
+                        o.quantityRemaining !== null
+                          ? o.quantityRemaining < quantity
+                          : false
+                      }
+                    >
+                      {o.additionalPrice
+                        ? t("option", {
+                            name: o.name,
+                            costUnit: moneyDisplay({
+                              amount: o.additionalPrice / 100,
+                              currency: merch.currency,
+                            }),
+                          })
+                        : o.name}
+                    </option>
+                  ))}
+              </SelectEl>
+              {formState.errors.merchOptionIds && (
+                <Box
+                  variant="warning"
+                  className={css`
+                    margin-top: 0.5rem;
+                  `}
+                  compact
+                >
+                  {formState.errors.merchOptionIds.message}
+                </Box>
+              )}
+            </FormComponent>
+          ))}
+        </div>
+        <BuyMerchItemDestinations merch={merch} />
+        <div
+          className={css`
+            border-bottom: 1px solid var(--mi-darken-x-background-color);
+            padding-bottom: 1rem;
+            margin-bottom: 2rem;
+          `}
         >
-          {t("goToCheckOut")}
-        </Button>
-      </div>
-      <div
-        className={css`
-          margin-top: 1rem;
-        `}
-      >
-        <small>{t("artistCheckoutPage")}</small>
-      </div>
-    </form>
+          <p
+            className={css`
+              margin: 1rem auto;
+              font-weight: bold;
+            `}
+          >
+            {t("orderTotal", {
+              amount: moneyDisplay({ amount: price, currency: merch.currency }),
+            })}
+          </p>
+          <IncludesDigitalDownload merch={merch} artist={artist} />
+        </div>
+
+        <FormComponent>
+          <label htmlFor="comment">{t("leaveAComment")}</label>
+          <TextArea id="comment" {...methods.register("message")} rows={2} />
+          <small>{t("messageToArtist")}</small>
+        </FormComponent>
+
+        <div
+          className={css`
+            display: flex;
+            justify-content: flex-end;
+            margin-bottom: 2rem;
+          `}
+        >
+          <Button
+            disabled={!methods.formState.isValid || exceedsAvailable}
+            isLoading={isLoadingStripe}
+            size="big"
+            rounded
+            endIcon={<FaChevronRight />}
+          >
+            {t("goToCheckOut")}
+          </Button>
+        </div>
+        <div
+          className={css`
+            margin-top: 1rem;
+          `}
+        >
+          <small>{t("artistCheckoutPage")}</small>
+        </div>
+      </form>
+    </FormProvider>
   );
 };
 
