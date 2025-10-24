d92068566ca040f8a502ae1dd3b21712e69c3760
Simon <simsamsomred@gmail.com>
2025-10-24 16:53:45 -0400

    feat: a user collection endpoint

---
+++ src/routers/v1/users/{userId}/collection.ts
@@ -0,0 +1,125 @@
+import { Prisma, User } from "@mirlo/prisma/client";
+import { Request, Response } from "express";
+import { userAuthenticated } from "../../../../auth/passport";
+import prisma from "@mirlo/prisma";
+import trackGroupProcessor from "../../../../utils/trackGroup";
+import { merge } from "lodash";
+import { isTrackGroup } from "../../../../utils/typeguards";
+
+type Params = {
+  userId: string;
+};
+
+export default function () {
+  const operations = {
+    GET: [userAuthenticated, GET],
+  };
+
+  async function GET(req: Request, res: Response) {
+    const { userId } = req.params as unknown as Params;
+    const { trackGroupId } = req.query as unknown as { trackGroupId: string };
+
+    const loggedInUser = req.user as User;
+    if (Number(userId) === Number(loggedInUser.id)) {
+      const where: Prisma.UserTrackGroupPurchaseWhereInput = {
+        userId: Number(userId),
+      };
+      if (trackGroupId) {
+        where.trackGroupId = Number(trackGroupId);
+      }
+      const tgPurchases = await prisma.userTrackGroupPurchase.findMany({
+        where,
+        include: {
+          trackGroup: {
+            include: {
+              artist: true,
+              cover: true,
+              tracks: {
+                orderBy: {
+                  order: "asc",
+                },
+              },
+            },
+          },
+          transaction: true,
+        },
+      });
+
+      const whereTrack: Prisma.UserTrackPurchaseWhereInput = {
+        userId: Number(userId),
+      };
+      if (trackGroupId) {
+        whereTrack.track = { trackGroupId: Number(trackGroupId) };
+      }
+
+      const trackPurchases = await prisma.userTrackPurchase.findMany({
+        where: whereTrack,
+        include: {
+          track: {
+            include: {
+              trackGroup: {
+                include: {
+                  artist: true,
+                  cover: true,
+                  tracks: {
+                    orderBy: {
+                      order: "asc",
+                    },
+                  },
+                },
+              },
+            },
+          },
+          transaction: true,
+        },
+      });
+
+      // combine both lists and sort by transaction created date (newest first)
+      const combined = [...tgPurchases, ...trackPurchases].sort((a, b) => {
+        return (a.transaction?.createdAt ?? 0) > (b.transaction?.createdAt ?? 0)
+          ? -1
+          : 1;
+      });
+
+      res.json({
+        results: combined,
+      });
+    } else {
+      res.status(401);
+      res.json({
+        error: "Invalid route",
+      });
+    }
+  }
+
+  GET.apiDoc = {
+    summary: "Returns user's purchased trackgroups",
+    parameters: [
+      {
+        in: "path",
+        name: "userId",
+        required: true,
+        type: "string",
+      },
+    ],
+    responses: {
+      200: {
+        description: "Trackgroups that belong to the user",
+        schema: {
+          type: "array",
+          items: {
+            $ref: "#/definitions/TrackGroupPurchase",
+          },
+        },
+      },
+      default: {
+        description: "An error occurred",
+        schema: {
+          additionalProperties: true,
+        },
+      },
+    },
+  };
+
+  return operations;
+}
