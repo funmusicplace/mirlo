8e8e3a29263231644bf11a7e2d81f8d6afc010b8
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2026-01-27 19:15:38 +0100

    removed bunch of "allow" stuff in the iframe due to them being "monstrosities"

--- client/src/components/TrackGroup/TrackGroupEmbed.tsx
+++ client/src/components/TrackGroup/TrackGroupEmbed.tsx
@@ -36,7 +36,6 @@ const Embed: React.FC<{
   const widgetText = `<iframe
     src="${src}"
     style="width:100%; height: 371px; border:0; border-radius: 4px; overflow:hidden;"
-    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
   ></iframe>`;
 
