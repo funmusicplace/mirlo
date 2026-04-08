6843e3eacec672b477ede9e9253acc647ac86937
timglorioso <8241942+timglorioso@users.noreply.github.com>
2026-04-08 18:35:56 -0400

    Use radio group for pricing mode in PriceAndSuch
    Also change appearance to segmented control for better visual indication that they are selectable, exclusive options

--- client/src/components/ManageArtist/ManageTrackGroup/AlbumFormComponents/PriceAndSuch.tsx
+++ client/src/components/ManageArtist/ManageTrackGroup/AlbumFormComponents/PriceAndSuch.tsx
@@ -47,6 +47,13 @@ const PriceAndSuch: React.FC<{
   const { user } = useAuthContext();
   const [isUpdatingPricingMode, setIsUpdatingPricingMode] =
     React.useState(false);
+  const pricingModeInputRefs: {
+    [K in PricingMode]: React.RefObject<HTMLInputElement>;
+  } = {
+    "free-or-donate": React.useRef<HTMLInputElement>(null),
+    paid: React.useRef<HTMLInputElement>(null),
+    "no-payments": React.useRef<HTMLInputElement>(null),
+  };
 
   const isAlbumGettable = watch("isGettable", false);
   const minPrice = Number(watch("minPrice") || 0);
@@ -144,6 +151,25 @@ const PriceAndSuch: React.FC<{
     [errorHandler, getValues, hasSuggestedPrice, savePricingState, setValue]
   );
 
+  const [didUpdatePricingMode, setDidUpdatePricingMode] = React.useState(false);
+
+  // Restore focus to selected input after disabling and re-enabling
+  React.useEffect(() => {
+    if (isUpdatingPricingMode) {
+      setDidUpdatePricingMode(true);
+    } else if (!isUpdatingPricingMode && didUpdatePricingMode) {
+      pricingModeInputRefs[selectedPricingMode].current?.focus();
+      setDidUpdatePricingMode(false);
+    }
+  }, [
+    isUpdatingPricingMode,
+    didUpdatePricingMode,
+    pricingModeInputRefs["free-or-donate"].current,
+    pricingModeInputRefs["paid"].current,
+    pricingModeInputRefs["no-payments"].current,
+    selectedPricingMode,
+  ]);
+
   const onToggleSuggestedPrice = React.useCallback(
     async (checked: boolean) => {
       if (!isAlbumGettable || selectedPricingMode === "no-payments") {
@@ -201,16 +227,34 @@ const PriceAndSuch: React.FC<{
 
   return (
     <FormSection>
-      <h2>{t("priceAndSuch")}</h2>
+      <h2 id="label-pricing">{t("priceAndSuch")}</h2>
       <div className="flex flex-col gap-2">
         <FormComponent>
-          <label>{t("pricing")}</label>
           <div
+            aria-labelledby="label-pricing"
             className={css`
-              display: grid;
-              grid-template-columns: repeat(3, minmax(0, 1fr));
-              gap: 0.75rem;
+              display: flex;
+
+              & input:focus-visible + label > button {
+                outline: 5px auto Highlight;
+                outline: 5px auto -webkit-focus-ring-color;
+              }
+
+              & label:first-of-type > button {
+                border-start-end-radius: 0;
+                border-end-end-radius: 0;
+              }
+
+              & label ~ label > button {
+                border-radius: 0;
+              }
+
+              & label:last-of-type > button {
+                border-start-end-radius: var(--mi-border-radius);
+                border-end-end-radius: var(--mi-border-radius);
+              }
             `}
+            role="group"
           >
             {[
               {
@@ -225,18 +269,34 @@ const PriceAndSuch: React.FC<{
               { key: "no-payments", label: t("pricingNoPayments") },
             ].map((pricing) => {
               const isSelected = selectedPricingMode === pricing.key;
+              const id = `pricing-${pricing.key}`;
               return (
-                <ArtistButton
-                  key={pricing.key}
-                  type="button"
-                  onClick={() =>
-                    onSelectPricingMode(pricing.key as PricingMode)
-                  }
-                  disabled={isUpdatingPricingMode}
-                  variant={isSelected ? "default" : "outlined"}
-                >
-                  {pricing.label}
-                </ArtistButton>
+                <React.Fragment key={pricing.key}>
+                  <input
+                    checked={isSelected}
+                    className="sr-only"
+                    disabled={isUpdatingPricingMode}
+                    id={id}
+                    name="pricing"
+                    onChange={() =>
+                      onSelectPricingMode(pricing.key as PricingMode)
+                    }
+                    ref={pricingModeInputRefs[pricing.key as PricingMode]}
+                    type="radio"
+                  />
+                  <label htmlFor={id}>
+                    <ArtistButton
+                      onClick={() =>
+                        onSelectPricingMode(pricing.key as PricingMode)
+                      }
+                      tabIndex={-1}
+                      type="button"
+                      variant={isSelected ? "default" : "outlined"}
+                    >
+                      {pricing.label}
+                    </ArtistButton>
+                  </label>
+                </React.Fragment>
               );
             })}
           </div>
