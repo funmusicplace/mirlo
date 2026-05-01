6ebe28df16a30a535fccd99963e02c54267bf71b
Simon <simsamsomred@gmail.com>
2026-05-01 18:23:07 -0400

    feat: store client id on track play

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,8 +1,14 @@
-import type { User as PrismaUser } from "@mirlo/prisma/client";
+import type {
+  Client as PrismaClient,
+  User as PrismaUser,
+} from "@mirlo/prisma/client";
 
 declare global {
   namespace Express {
     interface User extends PrismaUser {}
+    interface Request {
+      client?: PrismaClient;
+    }
   }
 }
 
