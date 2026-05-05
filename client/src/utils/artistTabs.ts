cb0d2c96daf8035a590c886b516690ac8094683a
Simon <simsamsomred@gmail.com>
2026-05-05 16:12:55 -0400

    feat: ability to re-arrange tabs

---
+++ client/src/utils/artistTabs.ts
@@ -0,0 +1,25 @@
+export type TabId = "roster" | "releases" | "posts" | "support" | "merch";
+
+export interface TabConfig {
+  id: TabId;
+  label: string;
+  visible: boolean;
+  to: string;
+  navLinkId?: string;
+  editTo?: string;
+  editAriaLabel?: string;
+}
+
+export function sortTabsByOrder(
+  tabs: TabConfig[],
+  tabOrder: TabId[] | undefined
+): TabConfig[] {
+  if (!tabOrder) return tabs;
+  return [...tabs].sort((a, b) => {
+    const aPos = tabOrder.indexOf(a.id);
+    const bPos = tabOrder.indexOf(b.id);
+    return (
+      (aPos === -1 ? tabs.length : aPos) - (bPos === -1 ? tabs.length : bPos)
+    );
+  });
+}
