6418e5a5a3c857e7597234e0debe88d451ffdbd9
LouisLouiseKay <77789607+LouisLouiseKay@users.noreply.github.com>
2026-05-28 17:38:22 +0200

    refactor: recommended albums responsive layout

--- client/src/components/TrackGroup/RecommendedAlbums.tsx
+++ client/src/components/TrackGroup/RecommendedAlbums.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
 import { useQuery } from "@tanstack/react-query";
-import { getReleaseUrl } from "utils/artist";
+import ArtistRouterLink from "components/Artist/ArtistButtons";
 import ImageWithPlaceholder from "components/common/ImageWithPlaceholder";
 import { queryPublicRecommendedTrackGroups } from "queries/trackGroups";
-import ArtistRouterLink from "components/Artist/ArtistButtons";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { getReleaseUrl } from "utils/artist";
 
 const RecommendedAlbums: React.FC<{
   trackGroupId: number;
   artist: Artist;
-}> = ({ trackGroupId, artist }) => {
+  centered?: boolean;
+}> = ({ trackGroupId, artist, centered }) => {
   const { t } = useTranslation("translation", {
     keyPrefix: "trackGroupDetails",
   });
@@ -23,14 +24,16 @@ const RecommendedAlbums: React.FC<{
   }
 
   return (
-    <div className="mt-12">
+    <div className={`mt-12 ${centered ? "text-center" : ""}`}>
       <h2>{t("artistRecommends", { artistName: artist.name })}</h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-4">
+      <div
+        className={`flex flex-wrap gap-6 mt-4 ${centered ? "justify-center" : ""}`}
+      >
         {recommendedTrackGroups.results.map((rec: TrackGroup) => (
           <ArtistRouterLink
             key={rec.id}
             to={getReleaseUrl(rec.artist, rec)}
-            className="no-underline! text-inherit hover:opacity-80 transition-opacity"
+            className="no-underline! text-inherit hover:opacity-80 transition-opacity w-[calc(50%-0.75rem)] md:w-52"
           >
             <div>
               <ImageWithPlaceholder
@@ -38,10 +41,12 @@ const RecommendedAlbums: React.FC<{
                 alt={rec.title ?? ""}
                 size={300}
               />
-              <p className="mt-2 font-bold overflow-hidden text-ellipsis whitespace-nowrap">
+              <p className="mt-2 text-sm font-bold overflow-hidden text-ellipsis whitespace-nowrap">
                 {rec.title}
               </p>
-              <p className="mt-1 text-gray-600 text-sm">{rec.artist?.name}</p>
+              <p className="mt-1 text-(--mi-button-color) text-xs">
+                {rec.artist?.name}
+              </p>
             </div>
           </ArtistRouterLink>
         ))}
